791bea32fbcc8b08332f98da6bd45dba6c4cf7f1
Mobin <mobin.seven@live.com>
2019-06-27 20:58:25 +0430

    DeviceManagement AGAIN.

---
+++ VinarishMvc/Views/DeviceManagement/ManageNavPages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VinarishMvc.DeviceManagement
+{
+    public static class ManageNavPages
+    {
+        public static string DevicePlaces => "DevicePlaces";
+
+        public static string DeviceTypes => "DeviceTypes";
+
+        public static string DeviceStatus => "DeviceStatus";
+
+        public static string DevicePlacesNavClass(ViewContext viewContext) => PageNavClass(viewContext, DevicePlaces);
+
+        public static string DeviceTypesNavClass(ViewContext viewContext) => PageNavClass(viewContext, DeviceTypes);
+
+        public static string DeviceStatusNavClass(ViewContext viewContext) => PageNavClass(viewContext, DeviceStatus);
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? Microsoft.AspNetCore.Http.Extensions.UriHelper.GetDisplayUrl(viewContext.HttpContext.Request);
+            return activePage.Contains( page) ? "active" : null;
+        }
+    }
+}
