2f85592dfec77747553c5236527c01e0b3e77f67
Mobin <mobin.seven@live.com>
2019-07-12 10:47:34 +0430

    added a simple ChartViewComponent to Admin Home view.

---
+++ VinarishMvc/Components/Chart.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VinarishMvc.Data;
+
+namespace VinarishMvc.ViewComponents
+{
+    public class ChartViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ChartViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _context.Reports.ToListAsync();
+            return View(items);
+        }
+    }
+}
