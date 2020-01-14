0611e86364566fcb4681182472f28d13b531d452
Mobin <mobin.seven@live.com>
2020-01-14 09:29:30 +0330

    Merged VinarishCsb

---
+++ VinarishLib/Services/DatabaseInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
+using VinarishLib.Models;
+
+namespace VinarishLib.Data
+{
+    public interface IDatabaseInitializer
+    {
+        Task SeedAsync();
+    }
+
+    public class DatabaseInitializer : IDatabaseInitializer
+    {
+        private readonly VinarishDbContext _context;
+
+        public DatabaseInitializer(VinarishDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            //await _context.Database.EnsureCreatedAsync();// https://github.com/aspnet/EntityFrameworkCore/issues/2874
+            //_context.GetService<IRelationalDatabaseCreator>().CreateTables();
+
+            await _context.Database.MigrateAsync().ConfigureAwait(false);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
